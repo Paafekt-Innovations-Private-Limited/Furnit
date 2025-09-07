fb6268758500f4801c26727a4798c4852e99ff88
Sitaramaswamy Ponnamanda <ramponnamanda92@gmail.com>
2025-09-07 08:44:51 +0530

    Initial Commit

---
+++ Furnit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Furnit
+//
+//  Created by Sitaramaswamy Ponnamanda on 07/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Furnit/FurnitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitApp.swift
+//  Furnit
+//
+//  Created by Sitaramaswamy Ponnamanda on 07/09/25.
+//
+
+import SwiftUI
+
+@main
+struct FurnitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
